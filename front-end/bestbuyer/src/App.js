91d59392220a0f36df93c7ebad076299763e232f
Ruan Miguel <ruanmiguelps@gmail.com>
2021-11-03 09:42:48 -0300

    [Layout]-Add header and footer

--- front-end/bestbuyer/src/App.js
+++ front-end/bestbuyer/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
-
+import Main from './Pages/Main'
 function App() {
   return (
     <div className="App">
-     Hello World!
+      <Main /> 
     </div>
   );
 }
---
+++ front-end/bestbuyer/src/Pages/Main.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+import "../Styles/Main.css"
+
+function Main( { Style } ) {
+  return (
+    <div className="overlay">
+      <Header Style="header"/>
+      <Footer Style="footer"/>
+    </div>
+  );
+}
+
+export default Main;
+
+Main.propTypes = {
+Style: PropTypes.string.isRequired,
+  
+};
